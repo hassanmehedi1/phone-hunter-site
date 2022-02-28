3ed0ba04926c98b98efef93026f8aa9f70d8ca4c
Mehedi Hassan <rehan51322@gmail.com>
2022-02-28 23:27:12 +0600

    4th commit

--- js/app.js
+++ js/app.js
@@ -18,19 +18,27 @@ const displaySearchResult = phones => {
    const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
     phones.forEach(phone => {
-      console.log(phone);
+      // console.log(phone);
       const div = document.createElement('div');
       div.classList.add('col');
       div.innerHTML = `
       <div class="card h-100">
-            <img src="${phone.image}" class="card-img-top w-100" alt="...">
+            <img src="${phone.image}" class="card-img-top w-60" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">${phone.brand}</p>
             </div>
-            <button onclick="loadPhoneDetail()" type="button" class="btn btn-primary">Explore-More</button>
+            <button onclick="loadPhoneDetail('${phone.slug}')" type="button" class="btn btn-primary">Explore-More</button>
         </div>
       `;
       searchResult.appendChild(div);
     })
+};
+
+// Single Phone Details 
+const loadPhoneDetail = id => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.data));
 }
