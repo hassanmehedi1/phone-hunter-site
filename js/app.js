cefcf52f6e2e981d052165be437ba02cfcc2611f
Mehedi Hassan <rehan51322@gmail.com>
2022-02-28 22:19:26 +0600

    3rd commit

--- js/app.js
+++ js/app.js
@@ -18,6 +18,19 @@ const displaySearchResult = phones => {
    const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
     phones.forEach(phone => {
-       console.log(phone);
+      console.log(phone);
+      const div = document.createElement('div');
+      div.classList.add('col');
+      div.innerHTML = `
+      <div class="card h-100">
+            <img src="${phone.image}" class="card-img-top w-100" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">${phone.brand}</p>
+            </div>
+            <button onclick="loadPhoneDetail()" type="button" class="btn btn-primary">Explore-More</button>
+        </div>
+      `;
+      searchResult.appendChild(div);
     })
 }
